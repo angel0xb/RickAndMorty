8e6744f6d2d4c16c8698282d39566697670279f1
Angel Rodriguez <angelrod1011@gmail.com>
2020-09-15 16:32:10 -0400

    Initial Commit

---
+++ RickAndMorty/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RickAndMorty
+//
+//  Created by Angel Rodriguez on 9/15/20.
+//  Copyright © 2020 AngelRodriguez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
